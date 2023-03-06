eb3e637616983508d19ace7a5f6af385c3d2b3f5
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-03-06 14:58:09 -0600

    Starting files

---
+++ Stay-on-Track/src/main/java/com/StayonTrack/StayonTrack/StayOnTrackApplication.java
@@ -0,0 +1,13 @@
+package com.StayonTrack.StayonTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayOnTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayOnTrackApplication.class, args);
+	}
+
+}
